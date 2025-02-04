7f94d47daaae12556f32ee8542776a9ec2dca727
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2025-02-04 14:10:32 -0700

    Installed gin, created test route and server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
-	fmt.Println("Hello world!")
+	r := gin.Default()
+
+	r.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"Message": "Success!"})
+
+		fmt.Println("Test route accessed.")
+	})
+
+	r.Run(":8080")
 }
