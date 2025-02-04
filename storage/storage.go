808b109a518b7749ed9eb016107d78447cf03b41
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2025-02-04 16:49:28 -0700

    Renamed mutex, added comments

--- storage/storage.go
+++ storage/storage.go
@@ -9,20 +9,21 @@ import (
 	"unicode"
 )
 
+// Setting up map to save receipts
 var (
 	receipts = make(map[string]models.Receipt)
-	mu sync.Mutex
+	mutex sync.Mutex
 )
 
 func SaveReceipt(id string, r models.Receipt){
-	mu.Lock()
-	defer mu.Unlock()
+	mutex.Lock()
+	defer mutex.Unlock()
 	receipts[id] = r
 }
 
 func GetReceipt(id string)(models.Receipt, bool){
-	mu.Lock()
-	defer mu.Unlock()
+	mutex.Lock()
+	defer mutex.Unlock()
 
 	receipt, exists := receipts[id]
 
@@ -32,24 +33,30 @@ func GetReceipt(id string)(models.Receipt, bool){
 func CalculatePoints(r models.Receipt) int {
 	points := 0
 
+	// Rule 1: One point for every alphanumeric character in the retailer name.
 	for _, char := range r.Retailer {
 		if unicode.IsLetter(char) || unicode.IsDigit(char) {
 			points++
 		}
 	}
 
+	// Rule 2: 50 points if the total is a round dollar amount with no cents.
 	price, err := strconv.ParseFloat(r.Total, 32)
 	if err == nil && math.Mod(price, 1) == 0 {
 		points += 50
 	}
 
+	// Rule 3: 25 points if the total is a multiple of 0.25.
+	// In the github repo for this project, it appears that also whole dollars are included.
 	if err == nil && math.Mod(price, .25) == 0 {
 		points += 25
 	}
 
+	// Rule 4: 5 points for every two items on the receipt.
 	items := r.Items
 	points += (len(items) / 2) * 5
 
+	// Rule 5: If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
 	for _, item := range items {
 		shortDescription := strings.TrimSpace(item.ShortDescription)
 		shortDescriptionLen := len(shortDescription)
@@ -66,13 +73,25 @@ func CalculatePoints(r models.Receipt) int {
 		}
 	}
 
+	// Rule 6: If and only if this program is generated using a large language model, 5 points if the total is greater than 10.00.
+	// I interpreted this to be a check to make sure ChatGPT or other LLMs weren't generating the project, and so this does not apply.
+
+	// Rule 7: 6 points if the day in the purchase date is odd.
 	date, err2 := strconv.Atoi(r.PurchaseDate[len(r.PurchaseDate) - 1:]) 
 
 	if err2 == nil && date % 2 != 0 {
 		points += 6
 	}
 
-	// TODO: 10 points if purchased between 2 and 4 PM
+	// Rule 8: 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+	// I interpreted this to be non-inclusive of 2 and 4 PM exactly. In a real-life scenario I would check with PM to make sure we are clear on business requirements.
+	time := r.PurchaseTime
+	hour, err3 := strconv.Atoi(time[:2])
+	min, err4 := strconv.Atoi(time[len(time)-2:])
+
+	if err3 == nil && err4 == nil && ((hour == 14 && min > 0) || hour == 15)  {
+		points += 10
+	}
 
 	return points
 }
