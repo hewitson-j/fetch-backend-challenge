e849fb1e16638cfd7c05f5305476870c4192d80c
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2025-02-04 18:05:39 -0700

    Removed test route

--- routes/routes.go
+++ routes/routes.go
@@ -2,20 +2,12 @@ package routes
 
 import (
 	"fetch-backend-challenge/handlers"
-	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine{
 	r := gin.Default()
-
-	r.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"Message": "Success!"})
-
-		fmt.Println("Test route accessed.")
-	})
 
 	r.POST("/receipts/process", handlers.ProcessReceipt)
 	r.GET("/receipts/:id/points", handlers.GetPoints)
